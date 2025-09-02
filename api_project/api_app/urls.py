641064430f964d0fe738983626b7b976d1a88877
Juan Felipe Fernandez <ferjuanfelipe7@gmail.com>
2025-09-02 12:27:18 -0500

    CRUD de Tareas y filtrado por fecha limite

--- api_project/api_app/urls.py
+++ api_project/api_app/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 from .views import (
-    PersonaList, PersonaByDocumento, ActualizarPersona
+    PersonaList, PersonaByDocumento, ActualizarPersona, CrearPersona, EliminarPersona,
+    TareaList, CrearTarea, ActualizarTarea, EliminarTarea, TareaByFechaLimite
 )
 
 urlpatterns = [
     path('personas/', PersonaList.as_view(), name='persona-list'),
-    path('personas/crear/', PersonaList.as_view(), name='persona-crear'),
+    path('personas/crear/', CrearPersona.as_view(), name='persona-crear'),
     path('personas/actualizar/<int:pk>/', ActualizarPersona.as_view(), name='persona-actualizar'),
     path('personas/documento/<str:documento>/', PersonaByDocumento.as_view(), name='persona-por-documento'),
+    path('personas/eliminar/<int:pk>/', EliminarPersona.as_view(), name='persona-eliminar'),
+    path('tareas/', TareaList.as_view(), name='tarea-list'),
+    path('tareas/crear/', CrearTarea.as_view(), name='tarea-crear'),
+    path('tareas/actualizar/<int:pk>/', ActualizarTarea.as_view(), name='tarea-actualizar'),
+    path('tareas/eliminar/<int:pk>/', EliminarTarea.as_view(), name='tarea-eliminar'),
+    path('tareas/fecha_limite/<str:fecha_limite>/', TareaByFechaLimite.as_view(), name='tarea-por-fecha-limite')
 ]
--- api_project/api_app/views.py
+++ api_project/api_app/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import get_object_or_404, render
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
-from .models import Persona
-from .serializers import PersonaSerializer
+from .models import Persona, Tarea
+from .serializers import PersonaSerializer, TareaSerializer
 
 class PersonaList(generics.ListCreateAPIView):
     queryset = Persona.objects.all()
@@ -44,6 +44,17 @@ class ActualizarPersona(generics.UpdateAPIView):
         serializers.is_valid(raise_exception=True)
         serializers.save()
         return Response({'success': True, 'detail': 'Persona actualizada correctamente.', 'data': serializers.data}, status=status.HTTP_200_OK)
+
+class EliminarPersona(generics.DestroyAPIView):
+    queryset = Persona.objects.all()
+    serializer_class = PersonaSerializer
+
+    def delete(self, request, pk):
+        
+        persona = get_object_or_404(Persona, pk=pk)
+        persona.delete()
+        return Response({'success': True, 'detail': 'Persona eliminada correctamente.'}, status=status.HTTP_204_NO_CONTENT)
+
 #Buscar Persona por documento
 class PersonaByDocumento(generics.ListAPIView):
     serializer_class = PersonaSerializer
@@ -54,3 +65,56 @@ class PersonaByDocumento(generics.ListAPIView):
             raise NotFound('No se encontro una persona con ese documento.')
         serializers = PersonaSerializer(persona)
         return Response({'success': True, 'detail': 'Persona encontrada', 'data': serializers.data}, status=status.HTTP_200_OK)
+    
+class TareaList(generics.ListCreateAPIView):
+    queryset = Tarea.objects.all()
+    serializer_class = TareaSerializer
+    
+    def list(self, request):
+        tareas = Tarea.objects.all()
+        serializer = TareaSerializer(tareas, many=True)
+        if not tareas:
+            raise NotFound("No se encontraron tareas.")
+        return Response({'success': True, 'detail': 'Listado de tareas', 'data': serializer.data}, status=status.HTTP_200_OK)
+    
+class CrearTarea(generics.CreateAPIView):
+    queryset = Tarea.objects.all()
+    serializer_class = TareaSerializer
+
+    def post(self, request):
+        serializer = TareaSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'success': True, 'detail': 'Tarea creada correctamente.', 'data': serializer.data}, status=status.HTTP_201_CREATED)
+    
+class ActualizarTarea(generics.UpdateAPIView):
+    queryset = Tarea.objects.all()
+    serializer_class = TareaSerializer
+
+    def put(self, request, pk):
+        tarea = get_object_or_404(Tarea, pk=pk)
+        serializer = TareaSerializer(tarea, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'success': True, 'detail': 'Tarea actualizada correctamente.', 'data': serializer.data}, status=status.HTTP_200_OK)
+    
+
+class EliminarTarea(generics.DestroyAPIView):
+    queryset = Tarea.objects.all()
+    serializer_class = TareaSerializer
+
+    def delete(self, request, pk):
+        tarea = get_object_or_404(Tarea, pk=pk)
+        tarea.delete()
+        return Response({'success': True, 'detail': 'Tarea eliminada correctamente.'}, status=status.HTTP_204_NO_CONTENT)
+    
+class TareaByFechaLimite(generics.CreateAPIView):
+    queryset = Tarea.objects.all()
+    serializer_class = TareaSerializer
+
+    def get(self, request, fecha_limite):
+        tarea = Tarea.objects.filter(fecha_limite=fecha_limite).first()
+        if not tarea:
+            raise NotFound('No se encontro una tarea con esa fecha limite.')
+        serializer = TareaSerializer(tarea)
+        return Response({'success': True, 'detail': 'Tarea encontrada', 'data': serializer.data}, status=status.HTTP_200_OK)
